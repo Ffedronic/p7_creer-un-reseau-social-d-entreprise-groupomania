131537951cb1f57216b2a4ca93c533672545032b
fedronic <fedronicfelix@gmail.com>
2021-11-19 00:40:02 +0100

    banner: navbar en cours

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import logo from './icon-left-font-monochrome-black.png';
-import './App.scss';
+import Banner from './Banner/Banner';
+import Sign from './components/Sign';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <img src={logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div>
+      <Banner/>
+      <Sign/>
     </div>
   );
 }
---
+++ src/components/Sign.js
@@ -0,0 +1,8 @@
+function Sign() {
+    return (
+      <h1>Formulaire de connexion :</h1>
+    );
+  }
+  
+  export default Sign;
+  
